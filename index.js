ec189d235e1dae1c3319e5773ecc9cda1d3b241e
LordHunBRO <lordhun610@gmail.com>
2022-09-22 09:40:31 +0300

    Добавил новый файл

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "Lord"
+console.log(`Hello ${1}`)
+console.log(`Hello ${"name"}`)
+console.log(`Hello ${name}`)
+let age = 17
+if(age == 17){
+    //какое то действие
+} else if( name == "Lord"){
+    //какое то действие
+} else{
+    //какое то действие
+}
